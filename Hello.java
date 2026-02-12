25d6262942e745c4867a58b3520190c397347f2d
SeonwooKim03 <seonwook@mtu.edu>
2026-02-12 10:10:51 -0500

    Fixed version

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hellooooooo {
+public class Hello {
   public static void main(String[] args) {
-       System.out.println("This will fail compile!"
+       System.out.println("This is fix!");
   }
 }
